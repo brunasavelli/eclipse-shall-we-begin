59b474f0e6d8340ed28fe97acdbd820429705c60
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-03-27 09:58:13 -0300

    first commit

---
+++ app/pages/initial/Home.jsx
@@ -0,0 +1,10 @@
+import styles from "./home.css"
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+    return(
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
