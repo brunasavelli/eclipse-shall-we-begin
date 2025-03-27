555f41a73932ba6243cb24a1e8776b999546c918
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-03-27 10:31:59 -0300

    feat: home on

---
+++ app/home/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.background}>
+            <h1>oie</h1>
+        </div>
+    );
+}
