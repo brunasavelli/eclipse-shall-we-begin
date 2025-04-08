abca4c08b3de40b98867e3055c83c7f9df22534d
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-04-08 13:36:56 -0300

    feat: fix footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,73 @@
 import React from "react";
+import FooterColumn from "./FooterColumn";
 import styles from "../styles/Footer.module.css";
+
 
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <div className={styles.logoArea}>
-                <img src="/logo-img.png" />
-                <h2>Eclipse</h2>
+            <div className={styles.footerUpper}>
+                <div className={styles.left}>
+                    <div className={styles.logoArea}>
+                        <img src="/logo-img.png" className={styles.logo} alt="Logo" />
+                        <h2>ECLIPSE</h2>
+                    </div>
+                    <p>Estilo para cada fase da sua vida</p>
+                    <p>
+                        A Eclipse é uma loja online que oferece produtos de alta qualidade e
+                        estilo.
+                    </p>
+                </div>
+
+                <FooterColumn
+                title={"Links Rápidos"}
+                texts={[
+                    {text: "Home"},
+                    {text: "Catálogo"},
+                    {text: "Ofertas"},
+                    {text: "Sobre nós"},
+                    {text: "Contato"}
+                ]}
+                />
+
+
+                <FooterColumn
+                title={"Categorias"}
+                texts={[
+                    {text: "Skate"},
+                    {text: "HipHop"},
+                    {text: "Punk"},
+                    {text: "Sport Life"},
+                    {text: "Surf"},
+                    {text: "Japanese"}
+                ]}
+                />
+
+
+                <FooterColumn
+                title={"Redes Sociais"}
+                texts={[
+                    {text: "Instagram", icon: "./instagram.png"},
+                    {text: "TikTok", icon: "./tiktok.png"},
+                    {text: "Twitter", icon: "./twitter.png"},
+                    {text: "YouTube", icon: "./youtube.png"},
+                ]}
+                />
+
             </div>
-            <p>Estilo para cada fase da sua vida</p>
-            <p className={styles.text}>A Eclipse é uma loja virtual que oferece produtos de alta qualidade e estilo para cada fase da sua vida. Com uma ampla variedade de produtos, incluindo roupas, acessórios, eletrônicos e outros itens.</p>
             <div className={styles.line}></div>
             <div className={styles.footerLower}>
-                <div className={styles.textLower1}>
-                    <p>Eclipse © 2025. Todos os direitos reservados.</p>
+                <div className={styles.left2}>
+                    <p>2025 © Eclipse. Todos os direitos reservados.</p>
                 </div>
-                <div className={styles.textLower2}>
-                    <p>Política de Privacidade</p>
-                    <p>Termos de Uso</p>
-                    <p>Política de Cookies</p>
+                <div className={styles.right}>
+                    <div className={styles.links}>
+                        <p>Política de Privacidade</p>
+                        <p>Termos de uso</p>
+                        <p>Política de cookies</p>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
---
+++ components/FooterColumn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+
+
+export default function FooterColumn({ title, texts }) {
+    return (
+        <div className={styles.columns}>
+            <h3 className={styles.title}>{title}</h3>
+            {texts.map((item, index) => (
+                <p key={index} className={styles.text}>
+                    {item.icon && <img src={item.icon} alt="icon" className={styles.icon} />}
+                    {item.text}
+                </p>
+            ))}
+        </div>
+    )
+}
