fed0b5c2dc0250c3ff61a80587410a676a7af70d
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-04-17 09:30:03 -0300

    feat: add metadata icon

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 			export const metadata = {
 				title: "ECLIPSE",
+				icons: {
+					icon: "/logo-img.png",
+				}
 			};
 			export default function RootLayout({ children }) {
 				return (
